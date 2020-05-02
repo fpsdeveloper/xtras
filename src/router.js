667fd3f2e064a51b4b7c4e22f5cf3e245e3101e3
Federico Schnabel <federico_schnabel@itrsa.com.ar>
2020-05-02 08:45:23 -0300

    agrega metodos http put y get

--- src/router.js
+++ src/router.js
@@ -1,3 +1,5 @@
+
+const modelos = require('./model');
 const data = require('./data');
 const winston = require('../config/winston')
 const express = require('express')
@@ -16,6 +18,41 @@ router.get("/entradas/:index",(req,res) => {
 
 router.post("/entradas",(req,res) => {
    winston.debug("Agrega Entrada"); 
+   
+   let entrada = Object.create(modelos.entrada);
+   entrada.proyecto = req.body.proyecto;
+   entrada.tarea = req.body.tarea; 
+    
+   data.entradas.push(entrada);
+
    res.status(201).send("Se agrego una entrada"); 
 });
+
+router.put("/entradas/:index",(req,res) => {
+    winston.debug(`actualiza entrada id: ${req.params.index}.`);
+
+    if (data.entradas.length <= parseInt(req.params.index)){
+        return res.status(404).send(`No se encuentra la entrada con el indice: ${req.params.index}.`); 
+    }
+    
+    let entrada = data.entradas[req.params.index];
+
+    entrada.proyecto = req.body.proyecto;
+    res.status(200).send(entrada);
+});
+
+router.delete("/entradas/:index",(req,res) => {
+    winston.debug(`borra la entrada id: ${req.params.index}.`);
+
+    if (data.entradas.length <= parseInt(req.params.index)){
+        return res.status(404).send(`No se encuentra la entrada con el indice: ${req.params.index}.`); 
+    }
+
+    let entrada = data.entradas[req.params.index];
+
+    data.entradas.splice(req.param.index,1);
+
+    res.send(`Se elimino la nota id: ${req.params.index}`);
+});
+
 module.exports = router;
